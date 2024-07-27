1575e2e95f79fe8db2ed9969f0d12d29abe5500f
ysyoon <ysyoon@ysyoonui-MacBookAir.local>
2024-07-27 10:45:44 +0900

    Update

--- main.ts
+++ main.ts
@@ -1,85 +1,40 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
-
-// Remember to rename these classes and interfaces!
+import { App, Editor, Plugin, PluginSettingTab, Setting, moment } from 'obsidian';
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 
 interface MyPluginSettings {
-	mySetting: string;
+	accessKeyId: string;
+	secretAccessKey: string;
+	region: string;
+	bucket: string;
 }
 
+
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+	accessKeyId: 'YOUR_ACCESS_KEY_ID',
+	secretAccessKey: 'YOUR_SECRET_ACCESS_KEY',
+	region: 'YOUR_REGION',
+	bucket: 'YOUR_BUCKET'
 }
+
 
 export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
+	s3: S3Client
 
 	async onload() {
 		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
+		this.addSettingTab(new MySettingTab(this.app, this));
+		this.s3 = new S3Client({
+			region: this.settings.region,
+			credentials: {
+				accessKeyId: this.settings.accessKeyId,
+				secretAccessKey: this.settings.secretAccessKey,
 			}
 		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.registerEvent(this.app.workspace.on('editor-paste', this.handlePaste))
 	}
 
 	onunload() {
-
 	}
 
 	async loadSettings() {
@@ -89,25 +44,43 @@ export default class MyPlugin extends Plugin {
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
+
+
+	handlePaste = async (e: ClipboardEvent, editor: Editor) => {
+		const files = e.clipboardData?.files;
+		if (!files) return;
+		if (files.length === 0) return;
+		for (let i = 0; i < files.length; i += 1) {
+			if (!files[i].type.startsWith('image')) return;
+		};
+
+		e.preventDefault();
+		for (let i = 0; i < files.length; i += 1) {
+			const file = files[i];
+			const url = await this.uploadFile(file);
+			editor.replaceSelection(`![](${url})`);
+		} 
+	}
+
+	async uploadFile(file: File): Promise<string> {
+		const fileExtension = file.name.split('.').pop();
+		const fileName = moment().format('YYYYMMDDHHmmssSS') + '.' + fileExtension;
+
+		const params = {
+			Bucket: this.settings.bucket,
+			Key: fileName,
+			Body: file,
+			ContentType: file.type,
+		};
+
+		const result = await this.s3.send(new PutObjectCommand(params));
+		const url = `https://${this.settings.bucket}.s3.${this.settings.region}.amazonaws.com/${fileName}`;
+		return url
+	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
+class MySettingTab extends PluginSettingTab {
 	plugin: MyPlugin;
 
 	constructor(app: App, plugin: MyPlugin) {
@@ -119,15 +92,41 @@ class SampleSettingTab extends PluginSettingTab {
 		const {containerEl} = this;
 
 		containerEl.empty();
+		containerEl.createEl('h2', {text: 'AWS S3 Bucket Settings'});
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('Access Key ID')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setValue(this.plugin.settings.accessKeyId)
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.accessKeyId = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('Secret Access Key')
+			.addText(text => text
+				.setValue(this.plugin.settings.secretAccessKey)
+				.onChange(async (value) => {
+					this.plugin.settings.secretAccessKey = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('Region')
+			.addText(text => text
+				.setValue(this.plugin.settings.region)
+				.onChange(async (value) => {
+					this.plugin.settings.region = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('Bucket')
+			.addText(text => text
+				.setValue(this.plugin.settings.bucket)
+				.onChange(async (value) => {
+					this.plugin.settings.bucket = value;
 					await this.plugin.saveSettings();
 				}));
 	}
